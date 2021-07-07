3fd9ceec1e979d144b42c15b752865380c6bc830
wcfx <wcfx.dev@gmail.com>
2021-07-07 08:55:51 -0300

    style: button modal criado e estilizado

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
-import { Container } from "./styles";
+import { Container, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionProps {
   isOpen: boolean;
@@ -31,6 +33,17 @@ export function NewTransactionModal({
 
         <input placeholder="Título" />
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saídas" />
+            <span>Saídas</span>
+          </button>
+        </TransactionTypeContainer>
 
         <input placeholder="Categoria" />
 
